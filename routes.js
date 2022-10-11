abbd226e2d49b7fd244b9c12b56e962c68e3eda7
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-10-11 15:00:45 +0530

    Update routes

--- routes.js
+++ routes.js
@@ -1,9 +1,4 @@
 import { nextRoutes } from "@layer0/next";
 import { Router } from "@layer0/core/router";
 
-export default new Router()
-  .noIndexPermalink()
-  .match("/service-worker.js", ({ serviceWorker }) => {
-    return serviceWorker(".next/static/service-worker.js");
-  })
-  .use(nextRoutes);
+export default new Router().use(nextRoutes);
