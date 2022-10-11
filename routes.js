e24495ad446b4605a79beff986091b4a25f04de8
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-10-11 14:59:26 +0530

    Remove not-required

--- routes.js
+++ routes.js
@@ -1,8 +1,6 @@
 import { nextRoutes } from "@layer0/next";
 import { Router } from "@layer0/core/router";
 
-nextRoutes.setEnforceTrailingSlash(true);
-
 export default new Router()
   .noIndexPermalink()
   .match("/service-worker.js", ({ serviceWorker }) => {
